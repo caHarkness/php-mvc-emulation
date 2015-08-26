95ae6d267c440a6ff44817db6f1f984e81bb7e96
caHarkness <caHarkness@gmail.com>
2015-08-25 22:50:04 -0700

    URI Parameter Summary

--- php/header.php
+++ php/header.php
@@ -2,6 +2,8 @@
 ?>
 
 <h1>Title</h1>
-<p>This text is part of the footer. This partial view will be rendered on every page.<p>
+<p>This text is part of the footer. This partial view will be rendered on every page. Below, you will see the parameters passed in the URI encoded in JSON.<p>
+
+<pre><?php echo json_encode($arrParameters, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES); ?></pre>
 
 <hr>
