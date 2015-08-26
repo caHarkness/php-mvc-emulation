69546f10050c1fbedf960f12cef31446ec7b556e
caHarkness <caHarkness@gmail.com>
2015-08-25 22:40:17 -0700

    Relative paths

--- php/css.php
+++ php/css.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Muli:700,300,600,800,400">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="css/styles.css">
